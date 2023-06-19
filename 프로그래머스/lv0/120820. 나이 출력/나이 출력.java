9bb166b7fe96184ad81ebcc7e7e69efe24853efa
trevivom <107251548+trevivom@users.noreply.github.com>
2023-06-19 22:29:31 +0900

    [level 0] Title: 나이 출력, Time: 0.03 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120820. 나이 출력/나이 출력.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int age) {
+        
+        if (age <= 0 || age > 120) {
+            
+        }
+        int answer = 2022 - age + 1;
+        return answer;
+    }
+}
