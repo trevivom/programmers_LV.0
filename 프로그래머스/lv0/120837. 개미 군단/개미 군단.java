27df6709169a8f8338d31b151dcbc836bb2a493c
trevivom <107251548+trevivom@users.noreply.github.com>
2023-07-28 13:14:01 +0900

    [level 0] Title: 개미 군단, Time: 0.02 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120837. 개미 군단/개미 군단.java
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.java
@@ -1,14 +1,12 @@
 class Solution {
     public int solution(int hp) {
-        int answer = 0; // 초기화.
+        int answer = 0;
         
-        answer = hp / 5; // 5로 최대한 적게 나누기.
+        answer = hp / 5;
         hp %= 5;
-        
-        answer += hp  / 3; // 3으로 최대한 적게 나누기.
+        answer += hp  / 3;
         hp %= 3;
-        
-        answer += hp; // 남은 값 1로 최대한 적게 나누기.
+        answer += hp / 1;
         return answer;
     }
 }
